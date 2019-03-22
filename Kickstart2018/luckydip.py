3e2abee617c36ddebef189b7b8774f14e9881488
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-03-23 02:03:36 +0800

    small and large 100%

--- Kickstart2018/luckydip.py
+++ Kickstart2018/luckydip.py
@@ -1,18 +1,42 @@
-def getInput():
+import itertools
+
+
+def getInputAndProcess():
+    res = []
     test_cases = int(input())
-    counter = 0
-    input_line_one = []
-    input_line_two = []
-    num_items_bag = []
-    redip_values = []
-    valueList = [[]]
 
-    while counter < test_cases:
-        input_line_one = int(input())
-        num_items_bag.append(input_line_one[0])
-        redip_values.append(input_line_one[1])
-        input_line_two = int(input())
-        valueList[counter].append(input_line_two)
-        counter += 1
+    for i_ in range(test_cases):
+        num_items_bag, redip_values = list(
+            map(int, input().strip().split(' ')))
+        valueList = list(map(int, input().strip().split(' ')))
+        res.append(expectedValue(num_items_bag, redip_values, valueList))
+    return res
 
-    return num_items_bag, redip_values, valueList
+
+def expectedValue(num_items_bag, redip_values, valueList):
+    if redip_values == 0:
+        return sum(valueList)/num_items_bag
+
+    valueList.sort()
+    accumulated = [0] + list(itertools.accumulate(valueList))
+    p = 0
+
+    element = [0]*(redip_values+1)
+    element[0] = sum(valueList)/num_items_bag
+
+    for i in range(1, redip_values+1):
+        while p < num_items_bag and valueList[p] <= element[i-1]:
+            p += 1
+        element[i] = (accumulated[-1]-accumulated[p] +
+                      element[i-1]*p)/num_items_bag
+
+    return element[-1]
+
+
+if __name__ == "__main__":
+
+    res = []
+    res = getInputAndProcess()
+
+    for i_, c in enumerate(res):
+        print('Case #%d: %.7f\n' % (i_+1, c))
