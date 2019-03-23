bde6d6e1431dd11564fdbd7bd2fac2b2c3312588
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-03-23 14:13:10 +0800

    kruskalsMST init

---
+++ Greedy Algorithms/kruskalsMST.py
@@ -0,0 +1,10 @@
+# Picking smallest weight edge that does not form a cycle
+
+
+def kruskalsMST(startTime, FinishTime):
+    n = len(FinishTime)
+
+    iterator = 0
+    print("The Value of iterator is: ", iterator)
+
+    for another_iterator in xrange(n):
