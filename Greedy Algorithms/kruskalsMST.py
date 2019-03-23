46cca1e1d11e5378f83bd08a00e6ec3a3c84f5b9
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-03-23 17:57:54 +0800

    kruskals done

--- Greedy Algorithms/kruskalsMST.py
+++ Greedy Algorithms/kruskalsMST.py
@@ -7,4 +7,14 @@ def kruskalsMST(startTime, FinishTime):
     iterator = 0
     print("The Value of iterator is: ", iterator)
 
-    for another_iterator in xrange(n):
+    for another_iterator in range(n):
+        if startTime[another_iterator] >= FinishTime[iterator]:
+            print(another_iterator)
+            iterator = another_iterator
+
+
+if __name__ == "__main__":
+    startTimeArray = [1, 4, 8, 3, 3, 2]
+    FinishTimeArray = [5, 7, 0, 9, 9, 6]
+
+    kruskalsMST(startTimeArray, FinishTimeArray)
